8e72c64f94dbb131300b06f0a3e76d814f858ca0
7ericany <lericany@gmail.com>
2023-05-04 10:37:47 -0400

    save local changes in EDA

--- EDA_metrics.R
+++ EDA_metrics.R
@@ -4,6 +4,7 @@ library(dplyr)
 library(car)
 library(ggplot2)
 library(ggcorrplot)
+library(corrplot)
 
 data <- as.data.frame(read.csv("imputation/imputed_data.csv"))
 target <- data$TARGET
@@ -30,25 +31,47 @@ ggplot(hat.df, aes(x=hat.x))+
 
 
 # how many to drop?
-length(hat.df$hat.x[hat.df$hat.x > cutoff])
+length(hat.df$hat.x[hat.df$hat.x > cutoff])  # 1564
 # drop
 indices <- which(hat.df$hat.x > cutoff)
 target <- target[-indices]
 data_new <- as.data.frame(data[-indices, ])
 
 ########################
+# Correlation heatmap
+########################
+# cor_matrix = as.data.frame(cor(data_new))
+data_new <- data_new[, !names(data_new) %in% c("FLAG_MOBIL", "FLAG_CONT_MOBILE","FLAG_DOCUMENT_21")]
+M <- cor(data_new)
+# head(round(M,3))
+corrplot(M, method="color", tl.cex = 0.3, tl.col="black",
+         col=colorRampPalette(c("darkblue","white","#C71A14"))(100))
+
+
+
+########################
+# Factor
+########################
+mod5 <- factanal(data_new, factors=5)
+lort=loadings(mod5)[,1:5]
+p = dim(data_new)[2]
+heat_plot_factor <- data.frame(row=rep(1:p, times=5), 
+                               col=rep(x=c('1', '2', '3', '4', '5'), each=p), 
+                               value=matrix(lort))
+
+ggplot(data=heat_plot_factor, aes(x=col, y=rev(row), fill=value)) + 
+  geom_tile() + 
+  scale_fill_gradient2(low = "#E68502", mid="white", high = "#E61302") +
+  theme_bw() +
+  scale_y_discrete(limits=rev(colnames(data_new)[1:p])) +
+  ylab("") + xlab("Factors")
+########################
 # POST-STEP for modeling
 ########################
 
-
-
-
-
-
-
 ################### Calculate VIF scores for each feature ######################
 # Note: this step should execute AFTER imputation
-cor_matrix = as.data.frame(cor(data, use='pairwise.complete.obs'))
+# cor_matrix = as.data.frame(cor(data, use='pairwise.complete.obs'))
 # FLAG_DOCUMENT_2
 # ggcorrplot(cor_matrix)
 
